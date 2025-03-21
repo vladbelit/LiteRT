07509f99a851b6d5402cee72f3db5c8655af412f
Vladimir Belitskiy <belitskiy@gmail.com>
2025-03-21 10:31:53 -0400

    Basic Windows building attempt.
    # Conflicts:
    #	ci/build_pip_package_with_bazel.sh
    
    # Conflicts:
    #	.bazelrc

---
+++ ci/win_wrapper.sh
@@ -0,0 +1,23 @@
+set -eux
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+bashg
+
+# Simply so that `set -u` doesn't throw
+export TENSORFLOW_TARGET=windows
+export BAZEL_CONFIG_FLAGS=
+export CUSTOM_BAZEL_FLAGS
+export BAZEL_FLAGS=
+export BAZEL_STARTUP_OPTIONS=
+
+# shellcheck disable=SC2155
+export NIGHTLY_RELEASE_DATE=$(TZ="America/Los_Angeles" date "+%Y%m%d")
+
+DOCKER_PYTHON_VERSION="${DOCKER_PYTHON_VERSION:-3.11}"
+
+export CI_BUILD_PYTHON="python${DOCKER_PYTHON_VERSION}"
+export HERMETIC_PYTHON_VERSION="${DOCKER_PYTHON_VERSION}"
+
+${CI_BUILD_PYTHON} -m pip install pip setuptools wheel
+
+bash "${SCRIPT_DIR}/build_pip_package_with_bazel.sh"
